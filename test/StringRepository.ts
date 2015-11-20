dd97d119fc9818f1a625b8ae8012163985e7c806
Ignatius <ignatius.inbox@gmail.com>
2015-11-20 13:24:58 +1100

    Created initial StringRepository files

---
+++ test/StringRepository.ts
@@ -0,0 +1,9 @@
+/// <reference path="./typings/mocha/mocha.d.ts" />
+
+import StringRepository from '../src/StringRepository';
+
+describe("Constructor", () => {
+	it("can be created", () => {
+		new StringRepository();
+	});
+});
