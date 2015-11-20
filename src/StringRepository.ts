9c132c752ee758bb970232020becd9830a730fa3
Ignatius <ignatius.inbox@gmail.com>
2015-11-20 14:58:31 +1100

    Simple nonpersistent saving

--- src/StringRepository.ts
+++ src/StringRepository.ts
@@ -2,17 +2,18 @@
 
 class StringRepository {
 	private filePath : string;
-	
+	private store: {[index: string] : string};
 	constructor(filePath: string){
 		this.filePath = filePath;
+		this.store = { };
 	}
 	
 	public saveByKey(key: string, value : string) : void {
-		
+		this.store[key] = value;
 	}
 	
 	public async fetchByKey(key: string) : Promise<string> {
-		return "";
+		return this.store[key];
 	}
 }
 
