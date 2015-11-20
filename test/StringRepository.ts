a85791aa2a3ad3266e3ec4e918eb4c8622c0a40b
Ignatius <ignatius.inbox@gmail.com>
2015-11-20 16:42:30 +1100

    added persistency test and refactored

--- test/StringRepository.ts
+++ test/StringRepository.ts
@@ -4,17 +4,55 @@
 import {expect} from 'chai';
 import StringRepository from '../src/StringRepository';
 
+interface Dictionary {
+	[index: string] : string
+}
+
+function getSomeKeysAndValues() : Dictionary {
+	var values : Dictionary = {};
+	for(var i : number = 0; i < 10; i++) {
+		var key : string = "foo" + i.toString();
+		var value : string = "bar" + i.toString();
+		values[key] = value;
+	}
+	return values;
+}
+
+async function populateRepository(repository : StringRepository, values: Dictionary)
+{
+	for (var key in values) {
+		await repository.saveByKey(key, values[key]);
+	}
+}
+
+function createRepository() : StringRepository {
+	return new StringRepository("foo");
+}
+
 describe("Constructor", () => {
 	it("can be created", () => {
-		new StringRepository("foo");
+		createRepository()
 	});
 	
-	it("can fetch after saving", async () => {
-		var KEY : string = "fookey";
-		var VALUE : string = "fooval";
-		var repository : StringRepository = new StringRepository("foo");
-		repository.saveByKey(KEY, VALUE);
-		var result :string = await repository.fetchByKey(KEY);
-		expect(result).to.equal(VALUE);
+	it("can fetch non-persitently after saving", async () => {
+		var values : Dictionary = getSomeKeysAndValues();
+		var repository : StringRepository = createRepository()
+		populateRepository(repository, values);
+
+		for (var key in values) {
+			var result: string = await repository.fetchByKey(key);
+			expect(result).to.equal(values[key]);
+		}
+	});
+	
+	it("can fetch persitently after saving", async () => {
+		var values : Dictionary = getSomeKeysAndValues();
+		var repository : StringRepository = createRepository()
+		populateRepository(repository, values);
+		repository = createRepository()
+		for (var key in values) {
+			var result: string = await repository.fetchByKey(key);
+			expect(result).to.equal(values[key]);
+		}
 	});
 });
