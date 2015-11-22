cf4a12cfb03aafa10b44ca7d125dbedc61ddd8db
Ignatius <ignatius.inbox@gmail.com>
2015-11-23 09:43:50 +1100

    Restructured type definition includes

--- src/StringRepository.ts
+++ src/StringRepository.ts
@@ -1,4 +1,7 @@
+/// <reference path="./typings/tsd.d.ts" />
 "use strict";
+
+import * as fs from 'fs';
 
 class StringRepository {
 	private filePath : string;
@@ -11,10 +14,20 @@ class StringRepository {
 	
 	public async saveByKey(key: string, value : string) : Promise<void> {
 		this.store[key] = value;
+		await this.persistKey(key);
 	}
 	
 	public async fetchByKey(key: string) : Promise<string> {
-		return this.store[key];
+		var result: string = this.store[key];
+		return Promise.resolve(result);
+	}
+	
+	private async persistKey(key: string) : Promise<void> {
+		await this.persistAll();
+	}
+	
+	private async persistAll() : Promise<void> {
+		return;
 	}
 }
 
