94b1847f1deb6c75e3fdb2094a561f4d7af9de18
Ignatius <ignatius@ignatius.network>
2015-12-08 12:06:18 -0500

    refactored serializer

--- src/Serializer.ts
+++ src/Serializer.ts
@@ -2,38 +2,78 @@
 import Entry from "./Entry";
 import EntryCollection from "./EntryCollection";
 import Serialized from "./Serialized";
+import EntryValue from "./EntryValue";
 
 
 class Serializer {
+	private entry: Entry;
+	private serialized : Serialized;
+	private currentPropertyName: string;
+	private currentValue: EntryValue;
+	
 	serialize(entry: Entry) : Serialized {
-		var serialized : Serialized = new Serialized();
-		var key: string;
-		for (key in entry) {
-			if(entry[key] instanceof Entry) {
-				var relationship : Entry = <Entry> entry[key];
-				serialized.entries.push({
-					property: key,
-					className: Object.getPrototypeOf(relationship).constructor.className,
-					id: relationship.id
-				});
-			}
-			else if (entry[key] instanceof EntryCollection) {
-				var collection : EntryCollection<Entry> = <EntryCollection<Entry>> entry[key];
-				serialized.collections.push({
-					property: key,
-					className: collection.getClassName(),
-					ids: collection.getIds()
-				});
-			}
-			else {
-				serialized.properties.push({
-					property: key,
-					value: entry[key]
-				});
-			}
-
+		this.entry = entry;
+		this.serialized = new Serialized();
+		this.serializeEntryProperties();
+		return this.serialized;
+	}
+	
+	private serializeEntryProperties() : void {
+		for (this.currentPropertyName in this.entry) {
+			this.currentValue = this.entry[this.currentPropertyName];
+			this.serializeCurrentProperty();
 		}
-		return serialized;
+	}
+		
+	serializeCurrentProperty() {
+		if(this.isRelationship(this.currentValue)) {
+			this.serializeRelationship(<Entry> this.currentValue);
+		}
+		else if (this.isCollection(this.currentValue)) {
+			this.serializeCollection(<EntryCollection<Entry>> this.currentValue);
+		}
+		else {
+			this.serializeProperty(this.currentValue)
+		}
+	}
+	
+	private isRelationship(value: EntryValue) {
+		var isRelationship: boolean = false;
+		if(value instanceof Entry) {
+			isRelationship = true;
+		}
+		return isRelationship;
+	}
+
+	private serializeRelationship (relationship: Entry) {
+		this.serialized.entries.push({
+			property: this.currentPropertyName,
+			className: Object.getPrototypeOf(relationship).constructor.className,
+			id: relationship.id
+		});
+	}
+	
+	private isCollection(value: EntryValue) {
+		var isCollection: boolean = false;
+		if(value instanceof EntryCollection) {
+			isCollection = true;
+		}
+		return isCollection;
+	}
+	
+	private serializeCollection (collection: EntryCollection<Entry>) {
+		this.serialized.collections.push({
+			property: this.currentPropertyName,
+			className: collection.getClassName(),
+			ids: collection.getIds()
+		});
+	}
+	
+	private serializeProperty(propertyValue : EntryValue) {
+		this.serialized.properties.push({
+			property: this.currentPropertyName,
+			value: propertyValue
+		});
 	}
 }
 
